62c95223383447d29190e6b234a11824f59ed62d
User <User@DESKTOP-7AV5KVT>
2020-07-24 03:11:24 +0530

    First commit uploading All files

---
+++ MyCode/src/demo/Alien.java
@@ -0,0 +1,12 @@
+package demo;
+
+
+// action -> Method  
+public class Alien {
+
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
---
+++ MyCode/src/demo/Allen.java
@@ -0,0 +1,7 @@
+package demo;
+
+
+// Action -> Method
+public class Allen {
+
+}
